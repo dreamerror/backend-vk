6c9e99de879a32d4dff9784845d346ff0d5a6255
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-06-18 22:52:55 +1000

    minor changes

--- get_album.py
+++ get_album.py
@@ -9,15 +9,27 @@ likes_url = "https://api.vk.com/method/likes.getList?owner_id=-197700721&type=ph
 users_url = "https://api.vk.com/method/users.get?user_ids={USER_ID}&access_token={TOKEN}&v=5.131"
 
 
-def get_photos():
+def get_photos_info():
     items = requests.get(album_url).json()['response']['items']
     result = list()
 
     for item in items:
         likes_count = requests.get(likes_url.format(ITEM_ID=item['id'], TOKEN=TOKEN)).json()['response']['count']
         user = requests.get(users_url.format(USER_ID=item['user_id'], TOKEN=TOKEN)).json()['response'][0]
-        meme_url = item['sizes'][0]['url']
-        yield (user['first_name'] + ' ' + user['last_name'], likes_count, meme_url)
+        meme_url = item['sizes'][-1]['url']
+        res = {
+            'author': user['first_name'] + ' ' + user['last_name'],
+            'group_id': -197700721,
+            'likes': likes_count,
+            'meme_url': meme_url,
+            'meme_id': item['id']
+        }
+        yield res
+
+
+def get_photos():
+    for item in get_photos_info():
+        yield item['author'], item['likes'], item['meme_url']
 
 
 if __name__ == "__main__":
