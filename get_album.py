1819f9262ac918f9a98fdc3fc577faec4e7e6935
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-06-18 22:01:48 +1000

    some optimisation

--- get_album.py
+++ get_album.py
@@ -8,9 +8,18 @@ album_url = f"https://api.vk.com/method/photos.get?owner_id=-197700721&album_id=
 likes_url = "https://api.vk.com/method/likes.getList?owner_id=-197700721&type=photo&item_id={ITEM_ID}&access_token={TOKEN}&v=5.131"
 users_url = "https://api.vk.com/method/users.get?user_ids={USER_ID}&access_token={TOKEN}&v=5.131"
 
-items = requests.get(album_url).json()['response']['items']
 
-for item in items:
-    likes_count = requests.get(likes_url.format(ITEM_ID=item['id'], TOKEN=TOKEN)).json()['response']['count']
-    user = requests.get(users_url.format(USER_ID=item['user_id'], TOKEN=TOKEN)).json()['response'][0]
-    print(user['first_name'], user['last_name'], likes_count)
+def get_photos():
+    items = requests.get(album_url).json()['response']['items']
+    result = list()
+
+    for item in items:
+        likes_count = requests.get(likes_url.format(ITEM_ID=item['id'], TOKEN=TOKEN)).json()['response']['count']
+        user = requests.get(users_url.format(USER_ID=item['user_id'], TOKEN=TOKEN)).json()['response'][0]
+        meme_url = item['sizes'][0]['url']
+        yield (user['first_name'] + ' ' + user['last_name'], likes_count, meme_url)
+
+
+if __name__ == "__main__":
+    for i in get_photos():
+        print(i)
